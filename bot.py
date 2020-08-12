07f41e20e53e5e3c13d4db763a781a25d13adefc
Apoorva Kumar <34601545+itsAPK@users.noreply.github.com>
2020-08-12 16:41:26 +0530

    Bot Code

---
+++ bot.py
@@ -0,0 +1,60 @@
+import requests
+import telebot
+import time
+from telebot.apihelper import ApiException
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton,ReplyKeyboardMarkup,ReplyKeyboardRemove,KeyboardButton
+token=""
+bot=telebot.TeleBot(token)
+
+'''server=Flask(__name__)'''
+
+def start_markup():
+    markup = ReplyKeyboardMarkup()
+    markup.row_width = 2
+    a=KeyboardButton('🔎 IP Lookup')
+    markup.row(a)
+    return markup
+
+@bot.message_handler(commands=['start'])
+def start_message(msg):
+    bot.send_chat_action(msg.chat.id, 'typing')
+    bot.send_message(msg.chat.id,'Hello ' + msg.from_user.first_name+"\nUse 🔎 *IP Lookup* to Find IP deatials",reply_markup=start_markup())
+
+@bot.message_handler(regexp='🔎 IP Lookup')
+def ip_handler(message):    
+    bot.send_chat_action(message.chat.id, 'typing')
+    sent = bot.send_message(message.chat.id, "Send IP address")
+    bot.register_next_step_handler(sent, ip)
+
+
+def ip(message):
+    ip=message.text
+    url='http://ip-api.com/json/{}?fields=country,countryCode,region,regionName,city,zip,lat,lon,timezone,isp'.format(ip)
+    r=requests.get(url).json()
+    bot.send_chat_action(message.chat.id, 'typing')
+    bot.send_message(message.chat.id,'fetching...')
+    try: 
+        country=r['country']
+        countryCode=r['countryCode']
+        region=r['region']
+        regionName=r['regionName']
+        city=r['city']
+        zip_=r['zip']
+        lat=r['lat']
+        lon=r['lon']
+        isp=r['isp']
+        timezone=r['timezone']
+        all_data=f'🚩*Details of* {message.text}\n\n🌐 *country :* {country}\n➖ *countryCode :* {countryCode}\n🏷 *region :* {region}\n🔺 *regionName :* {regionName} \n✅ *city :* {city}\n📍 *zipCode :* {zip_}\n📌 *latitude :* {lat}\n📌 *longitude :* {lon}\n⏰ *timezone :* {timezone}\n⚙️ *isp :* {isp}'
+        bot.send_chat_action(message.chat.id, 'typing')
+        bot.send_message(message.chat.id,all_data)
+    except KeyError:
+        bot.send_chat_action(message.chat.id, 'typing')
+        bot.send_message(message.chat.id,'❌ invalid IP address')
+
+
+
+while True:
+	try:
+		bot.infinity_polling(True)
+	except Exception:
+		time.sleep(1)
